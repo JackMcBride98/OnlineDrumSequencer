5520c92f1c7d7b2d7495e51a28fd5d27d024ec13
Jack McBride <37148941+Muccy98@users.noreply.github.com>
2020-06-16 15:06:29 +0100

    Add the initial server code

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static(__dirname));
+
+
+io.on('connection', function(socket){
+
+});
