396ac9b7ae9775ef96e8915415631487ae0a3444
Muccy98 <37148941+Muccy98@users.noreply.github.com>
2020-06-19 15:59:59 +0100

    the sound now plays back in a loop

--- main.js
+++ main.js
@@ -2,13 +2,38 @@
 $(function(){
     let socket = io();
     var playButton = $('#play');
+    var stopButton = $('#stop');
     var buttons = [$('#1'), $('#2'), $('#3'), $('#4')];
+    var buttonStates = [false,false,false,false];
+    var playing = false;
+    var timestep = 0;
     // var myMusic = $('#music');
     var myMusic = document.getElementById("music");
 
     playButton.click(function(){
-        console.log('play button clicked');
-        myMusic.play();
+        playing = true;
+        play(timestep);
+    })
+
+    function play(timestep){
+        if (buttonStates[timestep]){
+            myMusic.pause()
+            myMusic.currentTime = 0;
+            myMusic.play()
+            console.log((timestep+1) + ' kick' )
+        }
+        timestep++;
+        if(timestep > 3){
+            timestep = 0;
+        }
+        setTimeout(function(){
+            if(playing){play(timestep)}
+        }, 500)
+    }
+
+    stopButton.click(function(){
+        playing = false;
+        console.log('STAHP!') 
     })
     
     buttons.forEach(function(button){
@@ -27,11 +52,22 @@ $(function(){
     socket.on('button click', function(data){
         if (data.buttonState){
             buttons[data.buttonNumber-1].css('background', 'red');
+            buttonStates[data.buttonNumber-1] = true;
         }
         else{
             buttons[data.buttonNumber-1].css('background', 'white');
+            buttonStates[data.buttonNumber-1] = false;
         }
     })
 })
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
 
 //Not sure if we need to use JQuery
